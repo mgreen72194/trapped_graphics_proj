3e114027270aa1908b75133ade16bdf5ba215f1b
Jason Xu <jason-xu1996@hotmail.com>
2017-12-09 11:37:30 -0600

    Enables player to controll the sphere and camera move along with it

---
+++ Trapped/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    private float maxSpeed = 5f;
+    private Rigidbody rb;
+    private Vector3 input;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if (rb.velocity.magnitude <= maxSpeed)
+        {
+            rb.AddForce(input * moveSpeed);
+        }
+    }
+}
