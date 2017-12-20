a47c6c39ff43d0ab26c89d9acbeba1b53247a65a
Jason Xu <jason-xu1996@hotmail.com>
2017-12-20 16:07:30 -0600

    Added depth to the report

--- Trapped/Assets/Scripts/DayNightCycle.cs
+++ Trapped/Assets/Scripts/DayNightCycle.cs
@@ -3,11 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
